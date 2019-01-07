5bc590d56333c7e085a89701cb7bf985dfe7db4f
William Murphy <wmurphy@ventanex.com>
2019-01-07 11:18:15 -0600

    no message

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -3,12 +3,12 @@ class Api::V1::PeopleController < Api::V1::ApplicationController
   before_action :save_spot, only: [:index]
 
   def index
-    # TODO this needs to be fixed
     @people = User.where({
       gender: preferences(current_user),
       "allow_#{current_user.gender}".to_sym => true,
     }).where.not(id: Match.where(accepter_id: current_user.id).select(:asker_id))
       .where.not(id: Match.where(asker_id: current_user.id).select(:accepter_id))
+      .where.not(id: current_user.id)
       .page(@page)
 
     render json: @people,
